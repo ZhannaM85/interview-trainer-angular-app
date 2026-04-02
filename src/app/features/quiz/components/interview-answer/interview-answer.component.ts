c1ec6767f939cd5d503b9dcffbd13cffa775ff69
Zhanna M <zhanna.myshkovskaya@gmail.com>
2026-04-02 22:44:09 +0300

    [DAY-3] Added hamburger menu on mobile devices.

--- src/app/features/quiz/components/interview-answer/interview-answer.component.ts
+++ src/app/features/quiz/components/interview-answer/interview-answer.component.ts
@@ -1,18 +1,16 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 import type { Question } from '../../../../shared/models/question.model';
-import type { SelfRating } from '../../../../shared/models/self-rating.model';
 import { AnswerBlocksComponent } from '../../../../shared/components/answer-blocks/answer-blocks.component';
-import { SelfEvaluationComponent } from '../self-evaluation/self-evaluation.component';
 
 @Component({
     selector: 'app-interview-answer',
-    imports: [AnswerBlocksComponent, SelfEvaluationComponent],
+    imports: [AnswerBlocksComponent, TranslatePipe],
     templateUrl: './interview-answer.component.html',
     styleUrl: './interview-answer.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InterviewAnswerComponent {
     readonly question = input.required<Question>();
-    readonly rated = output<SelfRating>();
 }
